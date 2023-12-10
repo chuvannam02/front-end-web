7495b7d77f1cbc395f725448296f2a5c62db1086
Nam Chu Van <11202660@st.neu.edu.vn>
2023-12-10 18:12:21 +0700

    Update ProductForUser.jsx

--- src/components/ProductForUser/ProductForUser.jsx
+++ src/components/ProductForUser/ProductForUser.jsx
@@ -9,21 +9,7 @@ import { Footer } from "antd/es/layout/layout";
 import { Input, Pagination, Select, Alert, Spin } from "antd";
 import { Breadcrumb } from "antd";
 const { Option } = Select;
-// Define a service using a base URL and expected endpoints
-export const productService = createApi({
-  reducerPath: "productService",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-stack-backend-kw0h.onrender.com/api/v1" }),
-  endpoints: (builder) => ({
-    getAllProducts: builder.query({
-      query: (page = 1, limit = 5, searchTerm = "") => ({
-        url: "product-without-auth/all",
-        params: { page, limit, name: searchTerm },
-      }),
-    }),
-  }),
-});
-// Export hooks for usage in functional components
-export const { useGetAllProductsQuery } = productService;
+import { useGetAllProductsQuery } from "./ProductService";
 
 const ProductForUser = () => {
   const [searchTerm, setSearchTerm] = useState("");
