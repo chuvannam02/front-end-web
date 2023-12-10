2633fe65fa9568ee69e9c1838b12eed5f64fe904
Nam Chu Van <11202660@st.neu.edu.vn>
2023-12-10 18:20:16 +0700

    Update productService.jsx

--- src/components/ProductForUser/productService.jsx
+++ src/components/ProductForUser/productService.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const productService = createApi({
   reducerPath: "productService",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-stack-backend-kw0h.onrender.com/api/v1" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-stack-backend-kw0h.onrender.com/api/v1/" }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: (page = 1, limit = 5, searchTerm = "") => ({
